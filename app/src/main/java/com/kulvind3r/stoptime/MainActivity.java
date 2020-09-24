a8dd2325d6a93123bd4c66b482bf31a986ecaa8e
Kulvinder Singh <kulvind3r.singh@gmail.com>
2020-09-24 13:44:17 +0530

    Hello World

---
+++ app/src/main/java/com/kulvind3r/stoptime/MainActivity.java
@@ -0,0 +1,14 @@
+package com.kulvind3r.stoptime;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
